ed9da28eac1420067dff93428b075dbab4a5cc62
asepdarmawan <a.darmawan93@gmail.com>
2015-04-19 21:39:40 +0700

    Show Catalogs

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -21,11 +21,17 @@ class ProductTest < ActiveSupport::TestCase
 	assert_equal [I18n.translate('errors.messages.taken')], product.errors[:title]
   end
 
+  test "product title length minimum 5" do
+  	product = Product.new(title: "abc", description: "yyy", price: 1, image_url: "fred.gif")
+  	assert product.invalid?
+  	assert_equal ["is too short (minimum is 5 characters)"], product.errors[:title]
+  end
+
   test "product price must be positive" do
 	product = Product.new(title:"My Book Title", description: "yyy",image_url: "zzz.jpg")
 	product.price = -1
 	assert product.invalid?
-		assert_equal ["must be greater than or equal to 0.01"],product.errors[:price]
+		assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
 	
 	product.price = 0
 	assert product.invalid?
